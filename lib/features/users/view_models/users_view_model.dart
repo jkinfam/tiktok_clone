4db92502729dbe5e95f282bb74001cda41b4f04c
jkinfam <jk_in_fam@hanmail.net>
2024-01-14 18:13:16 +0900

    240113

--- lib/features/users/view_models/users_view_model.dart
+++ lib/features/users/view_models/users_view_model.dart
@@ -2,15 +2,27 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
 import 'package:tiktok_clone/features/users/models/user_profile_model.dart';
 import 'package:tiktok_clone/features/users/repos/user_repo.dart';
 
 class UsersViewModel extends AsyncNotifier<UserProfileModel> {
-  late final UserRepository _repository;
+  late final UserRepository _usersRepository;
+  late final AuthenticationRepository _authenticationRepository;
 
   @override
-  FutureOr<UserProfileModel> build() {
-    _repository = ref.read(userRepo);
+  FutureOr<UserProfileModel> build() async {
+    _usersRepository = ref.read(userRepo);
+    _authenticationRepository = ref.read(authRepo);
+
+    if (_authenticationRepository.isLoggedIn) {
+      final profile = await _usersRepository
+          .findProfile(_authenticationRepository.user!.uid);
+      if (profile != null) {
+        return UserProfileModel.fromJson(profile);
+      }
+    }
+
     return UserProfileModel.empty();
   }
 
@@ -20,14 +32,21 @@ class UsersViewModel extends AsyncNotifier<UserProfileModel> {
     }
     state = const AsyncValue.loading();
     final profile = UserProfileModel(
+      hasAvatar: false,
       bio: "undefined",
       link: "undefined",
       email: credential.user!.email ?? "anon@anon.com",
       uid: credential.user!.uid,
       name: credential.user!.displayName ?? "Anon",
     );
-    await _repository.createProfile(profile);
+    await _usersRepository.createProfile(profile);
     state = AsyncValue.data(profile);
+  }
+
+  Future<void> onAvatarUpload() async {
+    if (state.value == null) return;
+    state = AsyncValue.data(state.value!.copywith(hasAvatar: true));
+    await _usersRepository.updateUser(state.value!.uid, {"hasAvatar": true});
   }
 }
 
